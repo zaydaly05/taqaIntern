856a1289934ef7b83ebd4c4be83446819868952d
It Trainee <ittrainee@taqa.com.eg>
2025-07-08 15:05:58 -0700

    initial commit

---
+++ Models/Station.cs
@@ -0,0 +1,12 @@
+public class Station
+{
+public int AreaId{get; set;}
+public string Name {get; set;}
+public List<Station> Stations {get; set;}
+}
+
+//---------------code-----------------------------
+
+
+public DateTime ModifiedOn{get; set;}
+public stringBy{get; set;}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿buider.Services.AddEndpointsApiExplorer(); 
+buider.Services.AddSwaggerGen(); 
+if(app.Environment.IsDevelopment())
+{
+app.UseSwagger(); 
+app.UseSwaggerUI();
+}
