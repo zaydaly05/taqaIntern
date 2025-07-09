90fceb1c2869376dcc703eb1a1f4228b7633ab2f
ZaydAli <zaydaly05@gmail.com>
2025-07-09 11:03:12 -0700

    ..

--- Models/Station.cs
+++ Models/Station.cs
@@ -3,8 +3,8 @@ namespace InGazAPI.Models
 {
     public class Station
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int StationId { get; set; }
+        public string StationName { get; set; }
         public List<Station> Stations {get; set;}
         public DateTime ModifiedOn{get; set;}
         public string ModifiedBy{get; set;}
